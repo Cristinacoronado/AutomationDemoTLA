a50d49f2cae12ad79187e186f5379515c4854263
Cristinacoronado <Cristinacoronado68@gmail.com>
2020-08-16 20:46:56 -0400

    First commit

---
+++ src/test/Pages/HomePage.java
@@ -0,0 +1,28 @@
+package Pages;
+
+import Base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage {
+    WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(xpath = "//a[text()='User-Mgt']")
+    public WebElement userMgtBtn;
+
+    public String[] navButtons = {"Home", "Inputs", "Selectors", "Select-class", "Alert", "Pop-Up", "Multiple-window",
+            "Tables", "Calendar", "iFrames", "Action-class","JS-Executor","Synchronization", "Files", "User-Mgt"};
+
+public void clickNavBtn(String buttonName){
+    click(driver.findElement(By.linkText(buttonName)));
+}
+
+}
